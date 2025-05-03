e6a2c86590863c36e4a594442322b7c1ae43004d
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2025-05-04 00:14:58 +0530

    feat: Added secvices + testing done

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PollsModule } from './polls/pools.module';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), PollsModule],
