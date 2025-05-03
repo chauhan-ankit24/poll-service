3fec1b69221c0ea0430e0e0420fb261ef7d134f4
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2025-05-03 23:56:49 +0530

    feat: added polls controllers and tested

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PollsModule } from './polls/pools.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), PollsModule],
   controllers: [],
   providers: [],
 })
