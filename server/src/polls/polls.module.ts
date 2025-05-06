72dec71976c5b6cd8bb0ccd4d613da97ff0e8a27
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2025-05-07 00:07:01 +0530

    feature: added jwt service and tested

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
-import { redisModule } from 'src/modules.config';
+import { jwtModule, redisModule } from 'src/modules.config';
 import { PollsRepository } from './polls.repository';
 
 @Module({
-  imports: [ConfigModule, redisModule], // ConfigModule is imported to read environment variables provided by nestJs, redisModule is imported to use the Redis client.
+  imports: [ConfigModule, redisModule, jwtModule], // ConfigModule is imported to read environment variables provided by nestJs, redisModule is imported to use the Redis client.
   controllers: [PollsController],
   providers: [PollsService, PollsRepository],
 })
