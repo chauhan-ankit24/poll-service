81c9de98a602078e231767e7372567fcb37c3ea7
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2025-05-09 00:23:03 +0530

    feature: added multiple client connection and also tested 2-way communication with postman

--- server/src/polls/polls.gateway.ts
+++ server/src/polls/polls.gateway.ts
@@ -1,30 +1,45 @@
-// Import Logger from NestJS to write log messages (display info in the terminal)
 import { Logger } from '@nestjs/common';
-
-// Import decorators and interfaces needed for creating a WebSocket gateway
-import { OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
-
-// Import the PollsService which has the logic for handling poll-related tasks
+import {
+  OnGatewayInit,
+  WebSocketGateway,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Namespace, Socket } from 'socket.io';
 import { PollsService } from './polls.service';
 
-// Define a WebSocket gateway that communicates with clients in real-time
 @WebSocketGateway({
-  // This sets the part of the server that listens specifically for 'polls' messages
   namespace: 'polls',
 })
-
-// Create a class called PollsGateway to handle WebSocket connections for polls
-// We use OnGatewayInit to run code right after the gateway starts
-export class PollsGateway implements OnGatewayInit {
-  // Create a logger to show messages about what's happening inside this class
+export class PollsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   private readonly logger = new Logger(PollsGateway.name);
-
-  // The PollsService is made available here so we can use its methods inside this gateway
   constructor(private readonly pollsService: PollsService) {}
 
-  // This method is automatically called by NestJS after the gateway is ready to use
+  @WebSocketServer() io: Namespace;
+
+  // Gateway initialized (provided in module and instantiated)
   afterInit(): void {
-    // Log to the terminal that the WebSocket gateway has started successfully
     this.logger.log(`Websocket Gateway initialized.`);
+  }
+
+  handleConnection(client: Socket) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`WS Client with id: ${client.id} connected!`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+
+    this.io.emit('hello', `from ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`Disconnected socket id: ${client.id}`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+
+    // TODO - remove client from poll and send `participants_updated` event to remaining clients
   }
 }
